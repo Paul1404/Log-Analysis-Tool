41a6a88d4397d219e2f1fc0fa07c92713ab03228
Paul <97813342+Paul1404@users.noreply.github.com>
2023-07-17 22:58:53 +0200

    Update Get-LastSystemEvents.psm1

--- Get-LastSystemEvents.psm1
+++ Get-LastSystemEvents.psm1
@@ -1,28 +1,49 @@
 function Get-LastSystemEvents {
     <#
     .SYNOPSIS
-       Retrieves the last system events specified by the user and displays them in a grid view.
+    Retrieves the last system events specified by the user and displays them in a grid view.
     .DESCRIPTION
-       This function retrieves the last system events as specified by the user from the event log they choose. It creates a custom object for each event and displays them in a grid view using Out-GridView.
+    This function retrieves the last system events as specified by the user from the event log they choose. It creates a custom object for each event and displays them in a grid view using Out-GridView.
     .PARAMETER LogName
-       Specifies the event log name from which the events will be retrieved.
+    Specifies the event log name from which the events will be retrieved.
     .PARAMETER MaxEvents
-       Specifies the maximum number of events to retrieve.
+    Specifies the maximum number of events to retrieve.
     .PARAMETER Level
-       Specifies the level of the events to retrieve.
+    Specifies the level of the events to retrieve.
     .PARAMETER After
-       Specifies the start of the date range for the events to retrieve.
+    Specifies the start of the date range for the events to retrieve.
     .PARAMETER Before
-       Specifies the end of the date range for the events to retrieve.
+    Specifies the end of the date range for the events to retrieve.
+    .PARAMETER SortBy
+    Specifies the property to sort the events by.
+    .PARAMETER ExportPath
+    Specifies the path of the file where the events will be exported.
+    
     .EXAMPLE
-       Get-LastSystemEvents -LogName System -MaxEvents 10 -Level Error -After (Get-Date).AddDays(-7)
+    Get-LastSystemEvents -LogName System -MaxEvents 10 -Level Error -After (Get-Date).AddDays(-7)
+    
+    This example retrieves the last 10 error events from the System log that occurred within the last 7 days.
+    
+    .EXAMPLE
+    Get-LastSystemEvents -LogName Application -MaxEvents 50 -Before (Get-Date).AddHours(-1) -SortBy Provider
+
+    This example retrieves the last 50 events from the Application log that occurred in the last hour and sorts them by Provider name.
+    
+    .EXAMPLE
+    Get-LastSystemEvents -LogName Security -Level Critical -After (Get-Date).AddDays(-30) -SortBy ID
+
+    This example retrieves the critical events from the Security log that occurred in the last 30 days and sorts them by event ID.
     #>
+
+    
     param (
         [string]$LogName = "System",
         [int]$MaxEvents = 10,
         [ValidateSet('Critical', 'Error', 'Warning', 'Information', 'Verbose')][string]$Level,
         [DateTime]$After,
-        [DateTime]$Before
+        [DateTime]$Before,
+        [string]$ExportPath,
+        [ValidateSet('ID', 'Level', 'Provider', 'TimeCreated')][string]$SortBy = "TimeCreated"
     )
 
     # Build a hash table for the Get-WinEvent parameters
@@ -54,28 +75,47 @@ function Get-LastSystemEvents {
 
     $winEventParams['FilterXPath'] = $xpathFilter
 
-    # Get the system events
-    $events = Get-WinEvent @winEventParams
-
+    try {
+        # Get the system events
+        $events = Get-WinEvent @winEventParams
+    }
+    catch {
+        Write-Error -Message "Failed to retrieve system events. Detailed error: $_"
+        return
+    }
+    
     # Create an ArrayList to hold the event data
     $eventData = New-Object System.Collections.ArrayList
 
     foreach ($event in $events) {
-        # Create a custom object for each event
-        $eventObj = New-Object PSObject -Property @{
+        # Add the object to the array
+        $null = $eventData.Add([PSCustomObject]@{
             "ID"          = $event.Id
             "Level"       = $event.LevelDisplayName
             "Provider"    = $event.ProviderName
             "TimeCreated" = $event.TimeCreated
             "Message"     = $event.Message
-        }
-
-        # Add the object to the array
-        $null = $eventData.Add($eventObj)
+        })
     }
 
-    # Display the data in a new window
-    $eventData | Out-GridView
+    # Sort the array by the specified property
+    $eventData = $eventData | Sort-Object $SortBy
+
+    if ($PSBoundParameters.ContainsKey('ExportPath')) {
+        # Export the data to a CSV file
+        try {
+            $eventData | Export-Csv -Path $ExportPath -NoTypeInformation
+            Write-Output "Events have been successfully exported to $ExportPath."
+        }
+        catch {
+            Write-Error -Message "Failed to export events to $ExportPath. Detailed error: $_"
+            return
+        }
+    }
+    else {
+        # Display the data in a new window
+        $eventData | Out-GridView
+    }
 }
 
-Export-ModuleMember -Function Get-LastSystemEvents
+Export-ModuleMember -Function Get-LastSystem
