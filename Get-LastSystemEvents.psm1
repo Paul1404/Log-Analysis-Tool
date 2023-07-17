cccd35851941bb767f20502f555f331a2805378e
Paul <97813342+Paul1404@users.noreply.github.com>
2023-07-17 22:31:06 +0200

    Update Get-LastSystemEvents.psm1
    With this version, the user can choose which event log they want to view and how many events they want to retrieve. The -LogName parameter is for specifying the event log name and -MaxEvents parameter for the number of events. The default values are "System" and 10, respectively, if the user doesn't specify them.

--- Get-LastSystemEvents.psm1
+++ Get-LastSystemEvents.psm1
@@ -1,15 +1,23 @@
 function Get-LastSystemEvents {
     <#
     .SYNOPSIS
-       Retrieves the last 10 system events and displays them in a grid view.
+       Retrieves the last system events specified by the user and displays them in a grid view.
     .DESCRIPTION
-       This function retrieves the last 10 events from the System event log. It creates a custom object for each event and displays them in a grid view using Out-GridView.
+       This function retrieves the last system events as specified by the user from the event log they choose. It creates a custom object for each event and displays them in a grid view using Out-GridView.
+    .PARAMETER LogName
+       Specifies the event log name from which the events will be retrieved.
+    .PARAMETER MaxEvents
+       Specifies the maximum number of events to retrieve.
     .EXAMPLE
-       Get-LastSystemEvents
+       Get-LastSystemEvents -LogName System -MaxEvents 10
     #>
-    
-    # Get the last 10 System events
-    $events = Get-WinEvent -LogName System -MaxEvents 10
+    param (
+        [string]$LogName = "System",
+        [int]$MaxEvents = 10
+    )
+
+    # Get the system events
+    $events = Get-WinEvent -LogName $LogName -MaxEvents $MaxEvents
 
     # Create an ArrayList to hold the event data
     $eventData = New-Object System.Collections.ArrayList
